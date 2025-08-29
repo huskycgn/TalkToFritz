733c33d2bd864ccaae2e94f1effde10dc842926e
Joachim Lehmann <joachim.lehmann@googlemail.com>
2025-08-29 13:56:46 +0200

    beispiel cred.py hinzugefügt.

--- renametocred.py
+++ renametocred.py
@@ -4,7 +4,7 @@ PASS = "Passwort_dieses_Users"
 
 DB_HOST = "127.0.0.1"
 # Hostname oder IP Adresse der Postgresql Instanz
-DB = "name_der_datembank"
+DB = "name_der_datemnank"
 TABLE = "tabelle in der Datenbank"
 DB_USER = "datenbank_user"
 DB_PASS = "passwort_des_db_users"
