35c7c4df1e5cbc1e238c87b08c8a02bab5ed2c1b
Joachim Lehmann <joachim.lehmann@googlemail.com>
2025-08-29 13:54:16 +0200

    beispiel cred.py hinzugefügt.

--- funcs.py
+++ funcs.py
@@ -7,12 +7,22 @@ def get_ip_data():
     status = fc.call_action("WANIPConn1", "GetStatusInfo")["NewConnectionStatus"]
     uptime = fc.call_action("WANIPConn1", "GetStatusInfo")["NewUptime"]
     try:
-        address = fc.call_action("WANIPConn1", "GetExternalIPAddress")["NewExternalIPAddress"]
+        address = fc.call_action("WANIPConn1", "GetExternalIPAddress")[
+            "NewExternalIPAddress"
+        ]
     except IndexError:
         address = "0.0.0.0"
     upstream = fc.call_action("WANCommonInterfaceConfig", "GetCommonLinkProperties")[
-        "NewX_AVM-DE_UpstreamCurrentUtilization"].split(",")[0]
+        "NewX_AVM-DE_UpstreamCurrentUtilization"
+    ].split(",")[0]
     downstream = fc.call_action("WANCommonInterfaceConfig", "GetCommonLinkProperties")[
-        "NewX_AVM-DE_DownstreamCurrentUtilization"].split(",")[0]
-    ip_dict = {"status": status, "uptime": uptime, "address": address, "upstream": upstream, "downstream": downstream}
+        "NewX_AVM-DE_DownstreamCurrentUtilization"
+    ].split(",")[0]
+    ip_dict = {
+        "status": status,
+        "uptime": uptime,
+        "address": address,
+        "upstream": upstream,
+        "downstream": downstream,
+    }
     return ip_dict
---
+++ renametocred.py
@@ -0,0 +1,10 @@
+USER = "FritzboxuserName"
+# Admin reicht nicht, es muss ein extra User auf der Fritzbox angelegt werden!
+PASS = "Passwort_dieses_Users"
+
+DB_HOST = "127.0.0.1"
+# Hostname oder IP Adresse der Postgresql Instanz
+DB = "name_der_datembank"
+TABLE = "tabelle in der Datenbank"
+DB_USER = "datenbank_user"
+DB_PASS = "passwort_des_db_users"
