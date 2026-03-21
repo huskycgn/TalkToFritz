27eb2b3c10c940f8f3077dbdabbbce5869d2ecc1
Joachim Lehmann <joachim.lehmann@googlemail.com>
2026-03-21 02:04:56 +0100

    changed IP-Address

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from cred import *
 ip_data = get_ip_data()
 
 db_conn = psycopg2.connect(
-    host="192.168.178.117", user=DB_USER, password=DB_PASS, database="network"
+    host="192.168.100.1", user=DB_USER, password=DB_PASS, database="network"
 )
 cursor = db_conn.cursor()
 statement = f"INSERT INTO {TABLE} (status, uptime, ipaddr, ip6addr, upstream, downstream) VALUES ('{ip_data['status']}', {ip_data['uptime']}, '{ip_data['address']}','{ip_data['ipv6address']}', {ip_data['upstream']}, {ip_data['downstream']})"
